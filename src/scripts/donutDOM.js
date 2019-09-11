975bbbdf94e2194aa474dce5817824abb9f61d08
Madi Peper <madisonpeper@gmail.com>
2019-09-11 00:39:47 -0500

    removed finished code to start new for live code

--- src/scripts/donutDOM.js
+++
@@ -1,8 +0,0 @@
-import makeDonutHTMLComponent from "./donutHTML.js"
-
-// Function that adds donut HTML components to the DOM
-const addDonutToDOM = (donutObj) => {
-    document.querySelector(".donut-container").innerHTML += makeDonutHTMLComponent(donutObj)
-}
-
-export default addDonutToDOM
