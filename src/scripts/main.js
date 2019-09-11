0823a14123423fdc0ca9617f6829e6a7a90b668a
Madi Peper <madisonpeper@gmail.com>
2019-09-10 22:57:48 -0500

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,51 @@
+import API from "./DataManager.js"
+
+// make a dropdown of items from the API
+const makeTypesDropDown = () => {
+    API.getTypes().then(allTypes => {
+        // do I use makeElement and appendChild?
+        // or innerHTML?
+        allTypes.forEach(type => {
+
+        })
+    })
+}
+
+// Function that builds the donut
+const createNewDonut = (name, type, flavor, glaze, topping) => {
+    const newDonut = {
+        name: name,
+        type: type.name,
+        flavor: flavor.name,
+        glaze: glaze.name,
+        topping: topping.name
+    }
+    return newDonut
+}
+
+// Function that defines what a donut should look like in HTML
+const makeDonutHTMLComponent = (donut) => {
+    return `
+    <div class="donut">
+        <h3>${donut.name}</h3>
+        <h4>Type:</h4>
+        <p>${donut.type}</p>
+        <h4>Flavor:</h4>
+        <p>${donut.flavor}</p>
+        <h4>Glaze:</h4>
+        <p>${donut.glaze}</p>
+        <h4>Toppings:</h4>
+        <p>${donut.topping}</p>
+    </div>
+    `
+}
+
+// Function that adds donut HTML components to the DOM
+const addDonutToDOM = (donutObj) => {
+    document.querySelector(".donut-container").innerHTML += makeDonutHTMLComponent(donutObj)
+}
+
+addDonutToDOM(createNewDonut("This da best donut", types[2], flavors[0], glazes[4], toppings[1]))
+addDonutToDOM(createNewDonut("Only Okay", types[1], flavors[0], glazes[0], toppings[1]))
+addDonutToDOM(createNewDonut("DELICIOUS", types[0], flavors[0], glazes[0], toppings[0]))
+addDonutToDOM(createNewDonut("DA 💣", types[2], flavors[2], glazes[1], toppings[0]))
