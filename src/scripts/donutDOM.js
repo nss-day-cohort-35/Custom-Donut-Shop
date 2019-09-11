55746f2d9eeae66974c2bf9945d9ff21d66c4820
Madi Peper <madisonpeper@gmail.com>
2019-09-11 14:50:06 -0500

    switching to new branch for delete

--- src/scripts/donutDOM.js
+++ src/scripts/donutDOM.js
@@ -2,7 +2,7 @@ import makeDonutHTMLComponent from "./donutHTML.js"
 
 // Function that adds donut HTML components to the DOM
 const addDonutToDOM = (donutObj) => {
-    document.querySelectorAll(".donut-container")[1].innerHTML += makeDonutHTMLComponent(donutObj)
+    document.querySelectorAll(".donut--container")[1].innerHTML += makeDonutHTMLComponent(donutObj)
 }
 
 export default addDonutToDOM
