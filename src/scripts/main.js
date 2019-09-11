4b555213ee33cc95e7471b6467dff4c9040350f7
Madi Peper <madisonpeper@gmail.com>
2019-09-10 23:43:37 -0500

    added corrent imports

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,12 @@
 import Dropdown from "./dropdown.js"
-import 
+import createNewDonut from "./donut.js"
 
 Dropdown.makeTypesDropDown();
 Dropdown.makeFlavorsDropDown();
 Dropdown.makeGlazesDropDown();
 Dropdown.makeToppingsDropDown();
 
-
-
-
-
-
-
-// addDonutToDOM(createNewDonut("This da best donut", types[2], flavors[0], glazes[4], toppings[1]))
-// addDonutToDOM(createNewDonut("Only Okay", types[1], flavors[0], glazes[0], toppings[1]))
-// addDonutToDOM(createNewDonut("DELICIOUS", types[0], flavors[0], glazes[0], toppings[0]))
-// addDonutToDOM(createNewDonut("DA 💣", types[2], flavors[2], glazes[1], toppings[0]))
+addDonutToDOM(createNewDonut("This da best donut", types[2], flavors[0], glazes[4], toppings[1]))
+addDonutToDOM(createNewDonut("Only Okay", types[1], flavors[0], glazes[0], toppings[1]))
+addDonutToDOM(createNewDonut("DELICIOUS", types[0], flavors[0], glazes[0], toppings[0]))
+addDonutToDOM(createNewDonut("DA 💣", types[2], flavors[2], glazes[1], toppings[0]))
