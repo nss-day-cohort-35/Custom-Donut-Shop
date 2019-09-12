b8faac3f734d1f9b406b32510c325e6a25c1880f
Nate V <navogel@gmail.com>
2019-09-12 11:18:22 -0500

    Revert "Nv delete "

--- src/scripts/donutDOM.js
+++ src/scripts/donutDOM.js
@@ -1,17 +1,11 @@
 import donutHMTL from "./donutHTML.js"
 
-<<<<<<< HEAD
-// Function that adds donut HTML components to the DOM
-const addDonutToDOM = (donutObj) => {
-    document.querySelectorAll(".donut-container")[1].innerHTML += makeDonutHTMLComponent(donutObj)
-=======
 /*
     Responsible for putting our donuts on the DOM
 */
 
 const donutDOMBuilder = (donutObj) => {
     document.querySelector(".donut-container").innerHTML += donutHMTL(donutObj)
->>>>>>> master
 }
 
 export default donutDOMBuilder
